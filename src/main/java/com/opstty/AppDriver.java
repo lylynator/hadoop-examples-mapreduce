a3798b8843651edb6350251568d29246dcb2972e
Nguyen <florence.nguyen@efrei.net>
2021-10-11 11:56:46 +0200

    DistinctDistrict class

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -8,12 +8,13 @@ public class AppDriver {
     public static void main(String argv[]) throws Throwable {
         int exitCode = -1;
         ProgramDriver programDriver = new ProgramDriver();
-        programDriver.addClass("distinctDistricts", DistinctDistricts.class,
-                "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
 
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("distinctDistricts", DistinctDistricts.class,
+                    "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
+
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
